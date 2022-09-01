45339734c2690eb999dd2e2787e805f2e194168e
chrisjusel <christian00.vitale@gmail.com>
2022-09-01 18:53:39 +0200

    Maven project

---
+++ src/test/java/it/epicode/be/library/SpringLibraryApplicationTests.java
@@ -0,0 +1,13 @@
+package it.epicode.be.library;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLibraryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
